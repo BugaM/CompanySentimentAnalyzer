006476a94e8c6e40b7a2b39ada06eab5191425ff
Costa-SM <arthurcstevenson@gmail.com>
2023-04-06 00:33:14 -0300

    Implemented MR suggestions

--- scraper/TwitterScraper.py
+++ scraper/TwitterScraper.py
@@ -2,14 +2,16 @@ from AbstractScraper import AbstractScraper, NetworkUser, NetworkPost
 
 import snscrape.modules.twitter as sntwitter
 from datetime import datetime
+from typing import List
 
 #######################################################################################################################
 # Twitter Scraper Class ----------------------------------------------------------------------------------------------#
 #######################################################################################################################
 
 class TwitterScraper(AbstractScraper):
-    def __init__(self):
+    def __init__(self, tweet_cap):
         super().__init__()
+        self.tweet_cap = tweet_cap
     
     ###################################################################################################################
     # Methods that are specific to this class
@@ -31,16 +33,24 @@ class TwitterScraper(AbstractScraper):
         scraper_user = sntwitter.TwitterUserScraper(username)._get_entity()
         return self.parse_user(scraper_user)
     
-    def get_user_posts(self, username: str):
-        query = 'from: '
-        scraper_posts = sntwitter.TwitterSearchScraper()
 
-    def search_query(self, query: str):
+    def get_user_posts(self, username: str) -> List[NetworkPost]:
+        user = self.get_user(username)
+        
+        query = '(from:' + user.username + ')'
+        return self.search_query(query)
+
+
+    def search_query(self, query: str) -> List[NetworkPost]:
         tweets = []
 
         scraper_search = sntwitter.TwitterSearchScraper(query).get_items()
         
         for i, tweet in enumerate(scraper_search):
+            # Set a cap on the number of tweets in the list.
+            if i > self.tweet_cap:
+                break
+            
             tweets.append(NetworkPost(
                 content=tweet.rawContent,
                 author=self.parse_user(tweet.user),
@@ -51,7 +61,8 @@ class TwitterScraper(AbstractScraper):
         
         return tweets
 
-    def timed_search(self, search_string: str, start_date: datetime, end_date: datetime):
+
+    def timed_search(self, search_string: str, start_date: datetime, end_date: datetime) -> List[NetworkPost]:
         # Somebody will get this wrong at some point in time.
         # It may be me.
         if(start_date > end_date):
@@ -69,9 +80,8 @@ class TwitterScraper(AbstractScraper):
         scraper_search = sntwitter.TwitterSearchScraper(query).get_items()
         
         for i, tweet in enumerate(scraper_search):
-            # Set a cap of 500 tweets on the list.
-            # @TODO: Check if this is enough.
-            if i > 500:
+            # Set a cap on the number of tweets in the list.
+            if i > self.tweet_cap:
                 break
 
             tweets.append(NetworkPost(
