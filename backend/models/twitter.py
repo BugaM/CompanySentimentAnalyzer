93e2247b369b0ae7f2208cc9075607782786be3e
matheus-srm <msilvaramos84@gmail.com>
2023-06-21 20:35:30 -0300

    Fix fields of inference

--- backend/models/twitter.py
+++ backend/models/twitter.py
@@ -20,6 +20,7 @@ class TwitterPostInference:
     date: str
     source: str
     likes: str
+    label: int
     score: float
 
 
@@ -40,15 +41,3 @@ def from_query_to_twitter_posts(
         )
 
     return twitter_posts
-
-
-def from_query_to_twitter_post_inferences(
-    queried_post: List[Dict[str, Any]]
-) -> List[TwitterPostInference]:
-    twitter_post_inferences = []
-    for post in queried_post:
-        twitter_post_inferences.append(
-            TwitterPostInference(twitter_post=post["twitter_post"], score=post["score"])
-        )
-
-    return twitter_post_inferences
